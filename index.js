615dabf288b9ba1f172e82c26d2c1f48e476fd8d
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2023-12-28 15:10:44 +0700

    ini vercel

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+//   res.send('Hello, World!');
+  res.status(200).json({ message: 'Server successfully started', port });
 });
 
 app.listen(port, () => {
